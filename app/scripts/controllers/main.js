6f418c8ff00d34ccdadb355fcc229f6d7952e90b
Fender123 <pal.fender+git@gmail.com>
2014-07-06 19:53:18 +0200

    added tests for main controller and first filter test for formatList

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,59 +18,6 @@ angular.module('anCheckoutApp')
 
     //TODO: Zahleneingabe für gegebenes Geld Rückgeld
 
-    // $scope.buy = function (product, count) {
-    //     if (typeof count !== 'number') {
-    //         count = 1;
-    //     }
-    //     if ($scope.boughtProducts[product.name]) {
-    //         $scope.boughtProducts[product.name].count += count;
-    //     } else {
-    //         $scope.boughtProducts[product.name] = {
-    //             name: product.name,
-    //             price: product.price,
-    //             color: product.color,
-    //             count: count,
-    //             num: $scope.boughtProducts.length
-    //         };
-    //         $scope.boughtProducts.length++;
-    //     }
-    // };
-
-    // $scope.remove = function (product) {
-    //     if ($scope.boughtProducts[product.name]) {
-    //         $scope.boughtProducts[product.name].count--;
-    //         if ($scope.boughtProducts[product.name].count === 0) {
-    //             delete($scope.boughtProducts[product.name]);
-    //             $scope.boughtProducts.length--;
-    //         }
-    //     }
-    // };
-
-    // $scope.overallCount = function () {
-    //     var cnt = 0;
-    //     angular.forEach($scope.boughtProducts, function (p) {
-    //         if (p.name) {
-    //             cnt += p.count;
-    //         }
-    //     });
-    //     return cnt;
-    // };
-
-    // $scope.overallSum = function () {
-    //     var sum = 0;
-    //     angular.forEach($scope.boughtProducts, function (p) {
-    //         if (p.name) {
-    //             sum += p.count * p.price;
-    //         }
-    //     });
-    //     return sum;
-    // };
-
-    // $scope.removeAll = function () {
-    //     $scope.boughtProducts = {
-    //         length: 0
-    //     };
-    // };
 
     $scope.showTab = function (tab) {
         $scope.activeTab = tab;
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -6,17 +6,48 @@ describe('Controller: MainCtrl', function () {
   beforeEach(module('anCheckoutApp'));
 
   var MainCtrl,
-    scope;
+    scope,
+    cartService,
+    sceService;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $sce, products, cart) {
     scope = $rootScope.$new();
+    cartService = cart;
+    sceService = $sce;
     MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+      $scope: scope,
+      $sce: $sce,
+      products: products,
+      cart: cart
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('cart should be in scope', function () {
+    expect(scope.cart).toEqual(cartService);
+  });
+
+  it('products should be set', function(){
+    expect(scope.products).toBeDefined();
+  });
+
+  it('tab 2 should be set active', function(){
+    var newTab = 'Testtab';
+    scope.showTab(newTab);
+    expect(scope.activeTab).toBe(newTab);
+  });
+
+  it('string should not contain <br />', function(){
+    var name = 'test';
+    var formatted = scope.formatName(name);
+    expect(typeof formatted === 'object').toEqual(true); // confirms that it is an object, as should be the case when it has been through $sce.trustAsHtml
+    expect(formatted.$$unwrapTrustedValue().indexOf('<br />')).toBe(-1);
+  });
+
+  it('string should contain <br />', function(){
+    var name = 'test1,test2';
+    var formatted = scope.formatName(name);
+    expect(typeof formatted === 'object').toEqual(true); // confirms that it is an object, as should be the case when it has been through $sce.trustAsHtml
+    expect(formatted.$$unwrapTrustedValue()).toEqual('test1<br />test2');
   });
 });
