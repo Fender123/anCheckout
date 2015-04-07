dc36cb68faba5abe1055e65cd03201e322fb2325
Fender123 <pal.fender+git@gmail.com>
2015-04-07 09:34:22 +0200

    Updated products

--- app/scripts/services/products.js
+++ app/scripts/services/products.js
@@ -2,79 +2,79 @@
 
 (function(){
 	var products = [
-	  {
-	    'name': 'Fleischbrot,Salat,Weißwürste',
-	    'price': 4.30,
-	    'color': '#FFFF00',
-	    'group': 'Essen'
-	  },
-	  {
-	    'name': 'Pommes,Rettich',
-	    'price': 2.20,
-	    'color': '#FF9933',
-	    'group': 'Essen'
-	  },
-	  {
-	    'name': '1/2l Bier,1/4l Wein',
-	    'price': 3.20,
-	    'color': '#00FF00',
-	    'group': 'Essen'
-	  },
-	  {
-	    'name': '1/2 Hähnchen',
-	    'price': 5.90,
-	    'color': '#FFFFFF',
-	    'group': 'Essen'
-	  },
-	  {
-	    'name': 'Wurst',
-	    'price': 2.50,
-	    'color': '#66CCFF',
-	    'group': 'Essen'
-	  },
-	  {
-	    'name': '1l Bier',
-	    'price': 6.20,
-	    'color': '#CCFF99',
-	    'group': 'Essen'
-	  },
-	  {
-	    'name': 'Haxen',
-	    'price': 6.90,
-	    'color': '#FF3333',
-	    'group': 'Essen'
-	  },
-	  {
-	    'name': 'LKW',
-	    'price': 3.20,
-	    'color': '#FBC293',
-	    'group': 'Essen'
-	  },
-	  {
-	    'name': 'Schorle',
-	    'price': 2.80,
-	    'color': '#FFCCCC',
-	    'group': 'Essen'
-	  },
-	  {
-	    'name': 'Currywurst,Wilde Kartoffeln,Käse,Hering',
-	    'price': 2.80,
-	    'color': '#FFCCCC',
-	    'group': 'Essen'
-	  },
-	  {
-	    'name': 'Sprudel,A-Schorle,Cola,Fanta',
-	    'price': 2.50,
-	    'color': '#66CCFF',
-	    'group': 'Essen'
-	  },
-	  {
-	    'name': 'Pfand',
-	    'price': 2.00,
-	    'color': '#FFFF00',
-	    'group': 'Essen'
-	  }
-	];
+		  {
+		    "name": "Fleischbrot,Salat,WeiÃŸwÃ¼rste",
+		    "price": 4.30,
+		    "color": "#66CCFF",
+		    "group": "Essen"
+		  },
+		  {
+		    "name": "Pommes,Rettich",
+		    "price": 2.30,
+		    "color": "#FCDCC2",
+		    "group": "Essen"
+		  },
+		  {
+		    "name": "1/2l Bier,1/4l Wein",
+		    "price": 3.30,
+		    "color": "#FFABAB",
+		    "group": "Essen"
+		  },
+		  {
+		    "name": "1/2 HÃ¤hnchen",
+		    "price": 5.90,
+		    "color": "#CCFF99",
+		    "group": "Essen"
+		  },
+		  {
+		    "name": "Wurst",
+		    "price": 2.50,
+		    "color": "#FF9933",
+		    "group": "Essen"
+		  },
+		  {
+		    "name": "1l Bier",
+		    "price": 6.40,
+		    "color": "#FFFF00",
+		    "group": "Essen"
+		  },
+		  {
+		    "name": "Haxen",
+		    "price": 6.90,
+		    "color": "#FFFFFF",
+		    "group": "Essen"
+		  },
+		  {
+		    "name": "LKW",
+		    "price": 3.30,
+		    "color": "#FFABAB",
+		    "group": "Essen"
+		  },
+		  {
+		    "name": "Schorle",
+		    "price": 2.80,
+		    "color": "#00BD00",
+		    "group": "Essen"
+		  },
+		  {
+		    "name": "Currywurst,Wilde Kartoffeln,KÃ¤se,Hering",
+		    "price": 2.80,
+		    "color": "#00BD00",
+		    "group": "Essen"
+		  },
+		  {
+		    "name": "Sprudel,A-Schorle,Cola,Fanta",
+		    "price": 2.50,
+		    "color": "#FF9933",
+		    "group": "Essen"
+		  },
+		  {
+		    "name": "Pfand",
+		    "price": 2.00,
+		    "color": "#FF3333",
+		    "group": "Essen"
+		  }
+		];
 
 	angular.module('anCheckoutApp')
 	.factory('products', function(){
