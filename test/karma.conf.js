73966a8e5f1ad11dc821abba430093fe4fcbde68
Michael <pal.fender@gmail.com>
2015-04-10 08:35:28 +0200

    fixed tests, included new used modules

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -16,15 +16,21 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-touch/angular-touch.js',
-      'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
+		'bower_components/jquery/dist/jquery.js',
+		'bower_components/angular/angular.js',
+		'bower_components/angular-mocks/angular-mocks.js',
+		'bower_components/angular-resource/angular-resource.js',
+		'bower_components/angular-route/angular-route.js',
+		'bower_components/angular-sanitize/angular-sanitize.js',
+		'bower_components/angular-touch/angular-touch.js',
+		'bower_components/angular-route/angular-route.js',
+		'bower_components/jquery-ui/jquery-ui.js',
+		'bower_components/angular-ui-sortable/sortable.js',
+		'bower_components/jqueryui-touch-punch/jquery.ui.touch-punch.min.js',
+		'bower_components/angular-bootstrap-colorpicker/js/bootstrap-colorpicker-module.js',
+		'app/scripts/**/*.js',
+		'test/mock/**/*.js',
+		'test/spec/**/*.js'
     ],
 
     // list of files / patterns to exclude
