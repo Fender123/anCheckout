babea0b6125112df9364ffade6d633799039c964
Fender123 <pal.fender+git@gmail.com>
2016-03-21 18:52:28 +0100

    fixed tests (updated phantomjs)

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -59,7 +59,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: true,
+    singleRun: false,
 
     colors: true,
 
