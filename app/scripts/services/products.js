34b5186a1b39203249396120f49ddca6db7c0a78
Fender123 <pal.fender+git@gmail.com>
2017-04-17 14:50:36 +0200

    improved layout

--- app/scripts/services/products.js
+++ app/scripts/services/products.js
@@ -1,7 +1,7 @@
 'use strict';
 
-(function(){
-	var STORAGE_KEY = 'prds';
+(function () {
+  var STORAGE_KEY = 'prds';
 
   var COLOR_RED = '#FF3333';
   var COLOR_ROSE = '#FFABAB';
@@ -13,98 +13,116 @@
   var COLOR_LIGHTGREEN = '#CCFF99';
   var COLOR_WHITE = '#FFFFFF';
 
-	var products = [
-		  {
-		    'name': 'Fleischbrot,Salat,Weißwürste',
-		    'price': 4.50,
-		    'color': COLOR_BLUE,
-		    'group': 'Essen'
-		  },
-		  {
-		    'name': 'Pommes,Rettich',
-		    'price': 2.60,
-		    'color': COLOR_BEIGE,
-		    'group': 'Essen'
-		  },
-		  {
-		    'name': '1/2l Bier,1/4l Wein,LKW',
-		    'price': 3.30,
-		    'color': COLOR_ROSE,
-		    'group': 'Essen'
-		  },
-		  {
-		    'name': '1/2 Hähnchen',
-		    'price': 6.00,
-		    'color': COLOR_LIGHTGREEN,
-		    'group': 'Essen'
-		  },
-		  {
-		    'name': '1l Bier',
-		    'price': 6.40,
-		    'color': COLOR_YELLOW,
-		    'group': 'Essen'
-		  },
-		  {
-		    'name': 'Haxen',
-		    'price': 7.20,
-		    'color': COLOR_WHITE,
-		    'group': 'Essen'
-		  },
-		  {
-		    'name': 'Schorle / Pils,Currywurst,Wilde Kartoffeln,Käse',
-		    'price': 3.00,
-		    'color': COLOR_DARKGREEN,
-		    'group': 'Essen'
-		  },
-		  {
-		    'name': 'Sprudel / A-Schorle,Cola / Fanta,Wurst,Hering',
-		    'price': 2.80,
-		    'color': COLOR_ORANGE,
-		    'group': 'Essen'
-		  },
-		  {
-		    'name': 'Pfand',
-		    'price': 2.00,
-		    'color': COLOR_RED,
-		    'group': 'Essen'
-		  }
-		];
+  var products = [
+    {
+      'name': 'Haxen',
+      'price': 7.20,
+      'color': COLOR_WHITE,
+      'group': 'Essen'
+    },
+    {
+      'name': '1/2 Hähnchen',
+      'price': 6.00,
+      'color': COLOR_LIGHTGREEN,
+      'group': 'Essen'
+    },
+    {
+      'name': 'Fleischbrot,Salat,Weißwürste',
+      'price': 4.50,
+      'color': COLOR_BLUE,
+      'group': 'Essen'
+    },
+    {
+      'name': 'Pommes,Rettich',
+      'price': 2.60,
+      'color': COLOR_BEIGE,
+      'group': 'Essen'
+    },
+    {
+      'name': '1l Bier',
+      'price': 6.40,
+      'color': COLOR_YELLOW,
+      'group': 'Essen'
+    },
+    {
+      'name': '1/2l Bier,1/4l Wein,LKW',
+      'price': 3.30,
+      'color': COLOR_ROSE,
+      'group': 'Essen'
+    },
+    {
+      'name': 'Schorle / Pils,Currywurst,Wilde Kartoffeln,Käse',
+      'price': 3.00,
+      'color': COLOR_DARKGREEN,
+      'group': 'Essen'
+    },
+    {
+      'name': 'Cola / Fanta,Sprudel / A-Schorle,Wurst,Hering',
+      'price': 2.80,
+      'color': COLOR_ORANGE,
+      'group': 'Essen'
+    },
+    {
+      'name': 'Pfand',
+      'price': 2.00,
+      'color': COLOR_RED,
+      'group': 'Essen'
+    },
+    {
+      'name': '',
+      'price': 0,
+      'color': 'transparent',
+      'group': 'Essen'
+    },
+    {
+      'name': '',
+      'price': 0,
+      'color': 'transparent',
+      'group': 'Essen'
+    },
+    {
+      'name': '',
+      'price': 0,
+      'color': 'transparent',
+      'group': 'Essen'
+    }
+  ];
 
-	angular.module('anCheckoutApp')
-	.factory('products', function(){
-	    var prds = {
-	        length: 0
-	    };
-	    for (var i = 0; i < products.length; i++) {
-	        var p = products[i],
-	            group = p.group || 'Andere';
-	        if (!prds[group]) {
-	            prds[group] = {
-	                name: group,
-	                products: [],
-	                length: 0
-	            };
-	            prds.length++;
-	        }
-	        prds[group].length++;
-	        prds[group].products.push(p);
-	    }
-	    return {
-	    	getProducts: function(){
-				//Try to load products form localStorage
-			    var savePrds = localStorage.getItem(STORAGE_KEY);
-			    if(savePrds){
-			        return angular.fromJson(savePrds);
-			    }
-			    return angular.fromJson(angular.toJson(prds));
-	    	},
-	    	firstGroup: products[0].group,
-	    	resetProducts: function(){
-	    		localStorage.removeItem(STORAGE_KEY);
-	    	},
-	    	saveProducts: function(newPrds){
-		        localStorage.setItem(STORAGE_KEY, angular.toJson(newPrds));
-	    	}
-	    };
-	});
+  angular.module('anCheckoutApp')
+    .factory('products', function () {
+      var prds = {
+        length: 0
+      };
+      for (var i = 0; i < products.length; i++) {
+        var p = products[i],
+          group = p.group || 'Andere';
+        if (!prds[group]) {
+          prds[group] = {
+            name: group,
+            products: [],
+            length: 0
+          };
+          prds.length++;
+        }
+        prds[group].length++;
+        prds[group].products.push(p);
+      }
+      return {
+        getProducts: function () {
+          //Try to load products form localStorage
+          var savePrds = localStorage.getItem(STORAGE_KEY);
+          if (savePrds) {
+            return angular.fromJson(savePrds);
+          }
+          return angular.fromJson(angular.toJson(prds));
+        },
+        firstGroup: products[0].group,
+        resetProducts: function () {
+          localStorage.removeItem(STORAGE_KEY);
+        },
+        saveProducts: function (newPrds) {
+          localStorage.setItem(STORAGE_KEY, angular.toJson(newPrds));
+        }
+      };
+    });
 })();
