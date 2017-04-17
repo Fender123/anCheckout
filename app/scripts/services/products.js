3aa0c1c4efc7a80132b8a38ecac3d58d82c107b2
Fender123 <pal.fender+git@gmail.com>
2017-04-17 11:43:50 +0200

    modfied colors and prices for 2017

--- app/scripts/services/products.js
+++ app/scripts/services/products.js
@@ -3,77 +3,69 @@
 (function(){
 	var STORAGE_KEY = 'prds';
 
+  var COLOR_RED = '#FF3333';
+  var COLOR_ROSE = '#FFABAB';
+  var COLOR_YELLOW = '#FFFF00';
+  var COLOR_DARKGREEN = '#00BD00';
+  var COLOR_ORANGE = '#FF9933';
+  var COLOR_BEIGE = '#FCDCC2';
+  var COLOR_BLUE = '#66CCFF';
+  var COLOR_LIGHTGREEN = '#CCFF99';
+  var COLOR_WHITE = '#FFFFFF';
+
 	var products = [
 		  {
 		    'name': 'Fleischbrot,Salat,Weißwürste',
 		    'price': 4.50,
-		    'color': '#FFFF00',
+		    'color': COLOR_BLUE,
 		    'group': 'Essen'
 		  },
 		  {
 		    'name': 'Pommes,Rettich',
-		    'price': 2.50,
-		    'color': '#FF9933',
+		    'price': 2.60,
+		    'color': COLOR_BEIGE,
 		    'group': 'Essen'
 		  },
 		  {
-		    'name': '1/2l Bier,1/4l Wein',
+		    'name': '1/2l Bier,1/4l Wein,LKW',
 		    'price': 3.30,
-		    'color': '#00BD00',
+		    'color': COLOR_ROSE,
 		    'group': 'Essen'
 		  },
 		  {
 		    'name': '1/2 Hähnchen',
 		    'price': 6.00,
-		    'color': '#FFFFFF',
-		    'group': 'Essen'
-		  },
-		  {
-		    'name': 'Wurst,Hering',
-		    'price': 2.70,
-		    'color': '#66CCFF',
+		    'color': COLOR_LIGHTGREEN,
 		    'group': 'Essen'
 		  },
 		  {
 		    'name': '1l Bier',
 		    'price': 6.40,
-		    'color': '#CCFF99',
+		    'color': COLOR_YELLOW,
 		    'group': 'Essen'
 		  },
 		  {
 		    'name': 'Haxen',
-		    'price': 6.90,
-		    'color': '#FF3333',
+		    'price': 7.20,
+		    'color': COLOR_WHITE,
 		    'group': 'Essen'
 		  },
 		  {
-		    'name': 'LKW',
-		    'price': 3.30,
-		    'color': '#FCDCC2',
+		    'name': 'Schorle / Pils,Currywurst,Wilde Kartoffeln,Käse',
+		    'price': 3.00,
+		    'color': COLOR_DARKGREEN,
 		    'group': 'Essen'
 		  },
 		  {
-		    'name': 'Schorle,Pils',
-		    'price': 2.90,
-		    'color': '#FFABAB',
-		    'group': 'Essen'
-		  },
-		  {
-		    'name': 'Currywurst,Wilde Kartoffeln,Käse',
-		    'price': 2.90,
-		    'color': '#FFABAB',
-		    'group': 'Essen'
-		  },
-		  {
-		    'name': 'Sprudel,A-Schorle,Cola,Fanta',
-		    'price': 2.70,
-		    'color': '#66CCFF',
+		    'name': 'Sprudel / A-Schorle,Cola / Fanta,Wurst,Hering',
+		    'price': 2.80,
+		    'color': COLOR_ORANGE,
 		    'group': 'Essen'
 		  },
 		  {
 		    'name': 'Pfand',
 		    'price': 2.00,
-		    'color': '#FFFF00',
+		    'color': COLOR_RED,
 		    'group': 'Essen'
 		  }
 		];
